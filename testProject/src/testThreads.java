40890006557db0bdd41d941d8b1587bd431c8400
chemyp <chenyp@yp.com>
2019-05-29 09:57:17 +0800

    modified lock-test

--- testProject/src/testThreads.java
+++ testProject/src/testThreads.java
@@ -1,38 +1,46 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class testThreads {
     public static final String strA="a";
     public static final String strB="b";
+    public static final Lock lock1=new ReentrantLock();
+    public static final Lock lock2=new ReentrantLock();
     public static void main(String[] args) {
         Thread thread =new Thread(new Runnable() {
             @Override
             public void run() {
-                synchronized (strA){
+                lock1.lock();
                     System.out.println(Thread.currentThread().getName()+"获取a");
                     try {
                         Thread.sleep(200);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    synchronized (strB){
+                lock2.lock();
                         System.out.println(Thread.currentThread().getName()+"获取b");
-                    }
+                lock2.unlock();
+                lock1.unlock();
                 }
-            }
         });
         Thread threada =new Thread(new Runnable() {
             @Override
             public void run() {
-                synchronized (strB){
+                while(lock2.tryLock()){
                     System.out.println(Thread.currentThread().getName()+"获取b");
                     try {
                         Thread.sleep(200);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    synchronized (strA){
-                        System.out.println(Thread.currentThread().getName()+"获取a");
-                    }
                 }
-            }
+
+                lock1.lock();
+                        System.out.println(Thread.currentThread().getName()+"获取a");
+                lock1.unlock();
+                lock2.unlock();
+                }
+
         });
         thread.start();
         threada.start();
